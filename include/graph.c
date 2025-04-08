736bfd6407670c8d206bea720b36deb301824707
wscneto <contato.walter@proton.me>
2025-04-08 13:25:21 -0300

    checks for memory allocation errors and frees memory from deleted graph

--- include/graph.c
+++ include/graph.c
@@ -5,6 +5,11 @@
 Vertex* createVertex(int v)
 {
     Vertex* newVertex = malloc(sizeof(Vertex));
+    if(!newVertex)
+    {
+        printf("Erro na alocação de memória para o vértice.\n");
+        exit(EXIT_FAILURE);
+    }
     newVertex->vertex = v;
     newVertex->next = NULL;
 
@@ -13,15 +18,41 @@ Vertex* createVertex(int v)
 
 void deleteGraph(Graph* graph)
 {
+    if(!graph) return;
+
+    for(int i = 0; i < graph->numVertices; i++)
+    {
+        Vertex* curr = graph->adjLists[i];
+        
+        while(curr)
+        {
+            Vertex* temp = curr;
+            curr = curr->next;
+            free(temp);
+        }
+    }
+
+    free(graph->adjLists);
     free(graph);
 }
 
 Graph* createGraph(int numVertices, int isDirected)
 {
     Graph* graph = malloc(sizeof(Graph));
+    if(!graph)
+    {
+        printf("Erro na alocação de memória para grafo.\n");
+        exit(EXIT_FAILURE);
+    }
     graph->numVertices = numVertices;
     graph->isDirected = isDirected;
     graph->adjLists = malloc(numVertices * sizeof(Vertex*));
+    if(!graph->adjLists)
+    {
+        printf("Erro na alocação de memória para listas de adjacência.");
+        free(graph);
+        exit(EXIT_FAILURE);
+    }
 
     for(int i = 0; i < numVertices; i++)
     {
